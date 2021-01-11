0833f5c15783ca2c70b2d8fa932b2ba4344129c5
Lars Güttinger <lars.guettinger@wegmeister.com>
2021-01-11 11:13:07 +0100

    ClipTextSo Filled + Display Ready

--- Assets/Scripts/ScriptableObjects/ClipTextSO.cs
+++ Assets/Scripts/ScriptableObjects/ClipTextSO.cs
@@ -1,10 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
 
 namespace Timeline
 {
     [CreateAssetMenu(fileName = "defaultText", menuName = "SO/Timeline/Text", order = 0)]
     public class ClipTextSO : ScriptableObject
     {
-        
+        public string speakerName = "";
+        public string[] lines = new[] {""};
+
+        [Tooltip("Amount of Time (in sec) till one line appears completely")]
+        public float lineSpeed = 0;
+
+        public Color color = Color.white;
+
+        private void OnValidate()
+        {
+            lineSpeed = lineSpeed < 0 ? 0 : lineSpeed;
+        }
     }
 }
